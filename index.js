649cf02ef930b53ab77ee9d97a59b580fd2cf9bc
cloverzero <cloverzero@gmail.com>
2015-05-21 00:48:52 +0800

    添加bower依赖库

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var fs = require('fs');
-var ncp = require('ncp');
+var fse = require('fs-extra');
 var _ = require('underscore');
 var FrameBuilder = require('./builders/frame_builder');
 
@@ -14,9 +13,10 @@ var BASE_PATH = __dirname;
  * @constructor
  */
 function NyxParser(options) {
-    this.outputPath = process.cwd() + '/out';
     if (options && options.out) {
         this.outputPath = process.cwd() + "/" + options.out;
+    } else {
+        this.outputPath = process.cwd() + '/out';
     }
 
     this.frameBuilder = new FrameBuilder({
@@ -34,15 +34,16 @@ NyxParser.prototype = {
     build: function (config) {
         var self = this;
 
-            ncp(TEMPLATES_PATH + '/copies', self.outputPath, function (err) {
+            fse.copy(TEMPLATES_PATH + '/copies', self.outputPath, function (err) {
                 if (err) {
                     console.log(err);
                 }
+                console.log("copy files success!");
             });
 
-            fs.readFile(TEMPLATES_PATH + '/config.js', 'utf-8', function (err, content) {
+            fse.readFile(TEMPLATES_PATH + '/config.js', 'utf-8', function (err, content) {
                 var template = _.template(content);
-                fs.writeFile(self.outputPath + '/config.js', template({ json: JSON.stringify(config)}, function (err) {
+                fse.writeFile(self.outputPath + '/config.js', template({ json: JSON.stringify(config)}, function (err) {
                     if (err) {
                         console.log(err);
                     }
@@ -52,7 +53,7 @@ NyxParser.prototype = {
 
             try {
                 var result = self.frameBuilder.build(config);
-                fs.writeFileSync(self.outputPath + "/index.html", result);
+                fse.writeFileSync(self.outputPath + "/index.html", result);
                 console.log("build success!");
             } catch (e) {
                 console.log(e);
